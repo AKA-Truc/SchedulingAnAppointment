a6347167c286a2af3e3359f565ca9a589a1a7681
AKA-Truc <pthanhtruca3@gmail.com>
2025-06-29 02:44:19 +0700

    roles service and fix mongo db

--- src/service/service.controller.ts
+++ src/service/service.controller.ts
@@ -3,16 +3,19 @@ import { ServicesService } from "./service.service";
 import { CreateServiceDto } from "./dto/CreateService.dto";
 import { ApiQuery } from "@nestjs/swagger";
 import { Public } from "src/auth/guard/auth.guard";
+import { Roles } from "src/auth/guard/roles.guard";
 
 @Controller('services')
 export class ServicesController {
     constructor(private readonly service: ServicesService) { }
 
+    @Roles('ADMIN')
     @Post()
     create(@Body() dto: CreateServiceDto) {
         return this.service.create(dto);
     }
 
+    @Roles('ADMIN', 'DOCTOR', 'USER')
     @Public()
     @Get()
     @ApiQuery({ name: 'page', required: false, example: 1 })
@@ -26,12 +29,15 @@ export class ServicesController {
         return this.service.getAll(pageNumber, limitNumber);
     }
 
+    @Roles('ADMIN', 'DOCTOR', 'USER')
     @Public()
     @Get(':id')
     getService(@Param('id', ParseIntPipe) id: number) {
         return this.service.findById(id);
     }
 
+
+    @Roles('ADMIN')
     @Put(':id')
     update(
         @Param('id', ParseIntPipe) id: number,
@@ -39,6 +45,7 @@ export class ServicesController {
         return this.service.update(id, data);
     }
 
+    @Roles('ADMIN')
     @Delete(':id')
     delete(@Param('id', ParseIntPipe) id: number) {
         return this.service.delete(id);
